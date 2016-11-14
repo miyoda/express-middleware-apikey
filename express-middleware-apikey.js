9aa92a31e0c872a646ab52faaea5ea2e9ec68703
miyomahc <miyomahc@gmail.com>
2016-11-14 20:38:34 +0100

    fix options error

--- express-middleware-apikey.js
+++ express-middleware-apikey.js
@@ -2,7 +2,7 @@
 
 module.exports = function(api_keys) {
   return function(req, res, next) {
-      if (!options || !api_keys || api_keys.length != 0 && api_keys.indexOf(req.query.api_key) == -1) {
+      if (api_keys && api_keys.length != 0 && api_keys.indexOf(req.query.api_key) == -1) {
           console.log("Invalid api_key: "+req.query.api_key);
           res.status(401).send({ error: 'Invalid api_key' });
       } else {
